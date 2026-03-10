ec57aa0720bdcf148f58c1c3b4a48d6e4d75b564
Rameswari-06 <rameswaribarick@gmail.com>
2026-03-10 15:07:15 +0530

    UC2: Basic room types and static availability

--- BookMyStayApp.java
+++ BookMyStayApp.java
@@ -1,7 +1,44 @@
+abstract class Room {
+
+    String type;
+    int beds;
+    double price;
+
+    Room(String type, int beds, double price) {
+        this.type = type;
+        this.beds = beds;
+        this.price = price;
+    }
+
+    void displayDetails() {
+        System.out.println("Room Type: " + type);
+        System.out.println("Beds: " + beds);
+        System.out.println("Price: ₹" + price);
+    }
+}
+class SingleRoom extends Room {
+
+    SingleRoom() {
+        super("Single Room", 1, 2000);
+    }
+}
+class DoubleRoom extends Room {
+
+    DoubleRoom() {
+        super("Double Room", 2, 3500);
+    }
+}
+class SuiteRoom extends Room {
+
+    SuiteRoom() {
+        super("Suite Room", 3, 6000);
+    }
+}
 public class BookMyStayApp {
 
     public static void main(String[] args) {
         useCase1();
+        useCase2();
     }
 
     public static void useCase1() {
@@ -13,5 +50,30 @@ public class BookMyStayApp {
         System.out.println("Hotel Booking Management System");
         System.out.println("Version: " + version);
         System.out.println("=================================");
+    }
+
+
+    public static void useCase2() {
+
+        SingleRoom single = new SingleRoom();
+        DoubleRoom doubleRoom = new DoubleRoom();
+        SuiteRoom suite = new SuiteRoom();
+
+        int singleAvailable = 5;
+        int doubleAvailable = 3;
+        int suiteAvailable = 2;
+
+        System.out.println("Book My Stay - Hotel Booking System v2.0");
+
+        single.displayDetails();
+        System.out.println("Available: " + singleAvailable);
+        System.out.println();
+
+        doubleRoom.displayDetails();
+        System.out.println("Available: " + doubleAvailable);
+        System.out.println();
+
+        suite.displayDetails();
+        System.out.println("Available: " + suiteAvailable);
     }
 }
