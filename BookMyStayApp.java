11443612d7a702521f3f4a1a0813741f3286a5da
Rameswari-06 <rameswaribarick@gmail.com>
2026-03-10 14:52:38 +0530

    Feature usecase1

---
+++ BookMyStayApp.java
@@ -0,0 +1,17 @@
+public class BookMyStayApp {
+
+    public static void main(String[] args) {
+        useCase1();
+    }
+
+    public static void useCase1() {
+        String appName = "Book My Stay";
+        String version = "v1.0";
+
+        System.out.println("=================================");
+        System.out.println("Welcome to " + appName);
+        System.out.println("Hotel Booking Management System");
+        System.out.println("Version: " + version);
+        System.out.println("=================================");
+    }
+}
